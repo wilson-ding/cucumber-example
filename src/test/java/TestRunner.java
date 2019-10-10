ad1b2f3d931134f476371fe99b2ca233a9d00e83
Wilson Ding <wilson.ding@vonagebusiness.com>
2019-10-10 10:03:24 -0400

    Generate cucumber json report

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,7 +6,8 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(format={"pretty",
-        "html:build/test-results/test-report"},
+        "html:build/test-results/test-report",
+        "json:target/Destination/cucumber.json"},
         features={"src/test/resources"},
         glue = "",
         monochrome = true,
