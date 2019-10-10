63b48a8af0b0266b6303dffe27aac04bdedd3521
Wilson Ding <wilson.ding@vonagebusiness.com>
2019-10-10 09:38:13 -0400

    Supporting cucumber report

---
+++ src/test/java/UIExecutor.java
@@ -0,0 +1,13 @@
+/*
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "features", plugin = {"html:target/cucumber-html-report",
+        "json:target/cucumber.json", "pretty:target/cucumber-pretty.txt",
+        "junit:target/cucumber-results.xml"})
+public class UIExecutor {
+}
+*/
